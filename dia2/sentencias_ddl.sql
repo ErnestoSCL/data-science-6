8694dc6a45d8b6c643e5e70228bfcea76a3587fd
ErnestoSCL <ernestosaniel123@gmail.com>
2025-12-16 20:59:42 -0500

    mysql

---
+++ dia2/sentencias_ddl.sql
@@ -0,0 +1,17 @@
+-- SENTENCIAS DDL
+-- CREATE TABLE
+CREATE TABLE alumno(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nro_documento VARCHAR(20) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(100)
+);
+
+-- ALTERAR UNA TABLA
+ALTER TABLE alumno
+ADD COLUMN nota INT DEFAULT 0;
+
+-- ELIMINAR UNA TABLA
+DROP TABLE alumno;
+
+SELECT id,nro_documento,nombre,email,nota FROM alumno;
